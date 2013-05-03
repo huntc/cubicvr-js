67b766b7e4a6ae064be4851b75641fd20b83a692
Christopher Hunt <huntchr@gmail.com>
2013-05-03 11:47:34 +1000

    Revert "Requirejs configuration"
    This reverts commit 5bacb8474ec628961e11d9b8681b556de4ade56d.

--- src/main/resources/webjars-requirejs.js
+++
@@ -1,8 +0,0 @@
-/*global requirejs */
-
-// Ensure any request for this webjar brings in jQuery.
-requirejs.config({
-    shim: {
-        'CubicVR': [ 'webjars!ammo.js', 'webjars!pdf.js', 'webjars!jquery.js' ]
-    }
-});
